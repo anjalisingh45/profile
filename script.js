b8269684f9af03d6213120e8571e43b0e2c83a55
Anjali Singh <163300398+anjalisingh45@users.noreply.github.com>
2024-07-30 09:30:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function myMenuFunction(){
   
   /* ----- TYPING EFFECT ----- */
   let typingEffect = new Typed(".typedText",{
-    strings : ["Fullstack web Devloper.","Video editor."],
+    strings : ["Fullstack web Devloper.","Figma UI Design."],
     loop : true,
     typeSpeed : 100,
     backSpeed : 80,
